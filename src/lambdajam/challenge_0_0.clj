6243d73ab448d0d57d0f5a921196c7af4b2a8bfb
Michael Drogalis <mjd3089@rit.edu>
2015-06-13 13:52:23 -0700

    Make catalog adjustable.

--- src/lambdajam/challenge_0_0.clj
+++ src/lambdajam/challenge_0_0.clj
@@ -1,8 +1,6 @@
 (ns lambdajam.challenge-0-0
   (:require [lambdajam.workshop-utils :as u]))
 
-(def batch-size 20)
-
 ;;; Workflows ;;;
 
 (def workflow
@@ -11,28 +9,33 @@
 
 ;;; Catalogs ;;;
 
-(def catalog
-  [{:onyx/name :read-segments
-    :onyx/ident :core.async/read-from-chan
-    :onyx/type :input
-    :onyx/medium :core.async
-    :onyx/batch-size batch-size
-    :onyx/max-peers 1
-    :onyx/doc "Reads segments from a core.async channel"}
+(defn build-catalog
+  ([] (build-catalog 5 50))
+  ([batch-size batch-timeout]
+     [{:onyx/name :read-segments
+       :onyx/ident :core.async/read-from-chan
+       :onyx/type :input
+       :onyx/medium :core.async
+       :onyx/batch-size batch-size
+       :onyx/batch-timeout batch-timeout
+       :onyx/max-peers 1
+       :onyx/doc "Reads segments from a core.async channel"}
 
-   {:onyx/name :no-transform
-    :onyx/fn :lambdajam.challenge-0-0/no-transform
-    :onyx/type :function
-    :onyx/batch-size batch-size
-    :onyx/doc "Returns segments unchanged."}
+      {:onyx/name :no-transform
+       :onyx/fn :lambdajam.challenge-0-0/no-transform
+       :onyx/type :function
+       :onyx/batch-size batch-size
+       :onyx/batch-timeout batch-timeout
+       :onyx/doc "Returns segments unchanged."}
 
-   {:onyx/name :write-segments
-    :onyx/ident :core.async/write-to-chan
-    :onyx/type :output
-    :onyx/medium :core.async
-    :onyx/batch-size batch-size
-    :onyx/max-peers 1
-    :onyx/doc "Writes segments to a core.async channel"}])
+      {:onyx/name :write-segments
+       :onyx/ident :core.async/write-to-chan
+       :onyx/type :output
+       :onyx/medium :core.async
+       :onyx/batch-size batch-size
+       :onyx/batch-timeout batch-timeout
+       :onyx/max-peers 1
+       :onyx/doc "Writes segments to a core.async channel"}]))
 
 ;;; Functions ;;;
 
--- test/lambdajam/jobs/test_0_0.clj
+++ test/lambdajam/jobs/test_0_0.clj
@@ -19,14 +19,15 @@
    {:sentence "Sample inputs are easy to fabricate"}])
 
 (deftest test-level-0-challenge-0
-  (let [dev-env (component/start (onyx-dev-env (u/n-peers c/catalog c/workflow)))]
+  (let [catalog (c/build-catalog)
+        dev-env (component/start (onyx-dev-env (u/n-peers catalog c/workflow)))]
     (try 
       (let [dev-cfg (-> "dev-peer-config.edn" resource slurp read-string)
             peer-config (assoc dev-cfg :onyx/id (:onyx-id dev-env))
             lifecycles (c/build-lifecycles)]
         (u/bind-inputs! lifecycles {:read-segments input})
         (let [job {:workflow c/workflow
-                   :catalog c/catalog
+                   :catalog catalog
                    :lifecycles lifecycles
                    :task-scheduler :onyx.task-scheduler/balanced}]
           (onyx.api/submit-job peer-config job)
