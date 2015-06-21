327335e84da7fdad282892630a5b1d93f21c6265
Michael Drogalis <mjd3089@rit.edu>
2015-06-21 12:00:16 -0700

    4/3

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Onyx workshop materials for the LambdaJam 2015 conference. This workshop assumes
 
 This repository contains a set of tests that can be invoked with `lein test`. The workshop is divided into levels and challenges. Each level is dedicated to a particular Onyx feature, and is meant to focus your attention in one or two points of the code to avoid feeling overwhelmed. Each level has several challenges that you can work through. Every challenge is self contained in its own test (named `test_<level>_<challenge>.clj` and implementation file (named `challenge_<level>_<challenge>`). Every level has a "challenge 0". This challenge is already-working, and is a basic example of how to use the feature under discussion to help you get started. It should be useful as a base for experimentation if you want to start from a known-good state and push outwards into new ideas.
 
+Heads up! Some of these tests capture standard out using `clojure.core/with-out-str` to make verifying assertions as easy as possible. If you're wondering where your printlns are going, check the test harness for the challenge. Feel free to drop it while you debug.
+
 ### Guide
 
 - Level 0: Sanity Check
---
+++ src/lambdajam/challenge_4_3.clj
@@ -0,0 +1,100 @@
+(ns lambdajam.challenge-4-3
+  (:require [lambdajam.workshop-utils :as u]))
+
+;;; Workflows ;;;
+
+(def workflow
+  [[:read-segments :identity]
+   [:identity :write-segments]])
+
+;;; Catalogs ;;;
+
+(defn build-catalog
+  ([] (build-catalog 5 50))
+  ([batch-size batch-timeout]
+     [{:onyx/name :read-segments
+       :onyx/ident :core.async/read-from-chan
+       :onyx/type :input
+       :onyx/medium :core.async
+       :onyx/batch-size batch-size
+       :onyx/batch-timeout batch-timeout
+       :onyx/max-peers 1
+       :onyx/doc "Reads segments from a core.async channel"}
+
+      {:onyx/name :identity
+       :onyx/fn :lambdajam.challenge-4-3/my-identity-fn
+       :onyx/type :function
+       :onyx/batch-size batch-size
+       :onyx/batch-timeout batch-timeout
+       :onyx/doc "Returns the segment"}
+
+      {:onyx/name :write-segments
+       :onyx/ident :core.async/write-to-chan
+       :onyx/type :output
+       :onyx/medium :core.async
+       :onyx/batch-size batch-size
+       :onyx/batch-timeout batch-timeout
+       :onyx/max-peers 1
+       :onyx/doc "Writes segments to a core.async channel"}]))
+
+;;; Functions ;;;
+
+(defn my-identity-fn [state segment]
+  (swap! state + (:n segment))
+  segment)
+
+;;; Lifecycles ;;;
+
+(def logger (agent nil))
+
+;; <<< BEGIN FILL ME IN >>>
+
+(defn inject-state [event lifecycle]
+  (let [state (atom 0)]
+    {:challenge/state state
+     :onyx.core/params [state]}))
+
+(defn write-max-segment [event lifecycle]
+  (send logger (fn [_] (println "Summation was:" @(:challenge/state event))))
+  {})
+
+(defn inject-reader-ch [event lifecycle]
+  {:core.async/chan (u/get-input-channel (:core.async/id lifecycle))})
+
+(defn inject-writer-ch [event lifecycle]
+  {:core.async/chan (u/get-output-channel (:core.async/id lifecycle))})
+
+(def aggregate-lifecycle
+  {:lifecycle/before-task-start inject-state
+   :lifecycle/after-task-stop write-max-segment})
+
+(def reader-lifecycle
+  {:lifecycle/before-task-start inject-reader-ch})
+
+(def writer-lifecycle
+  {:lifecycle/before-task-start inject-writer-ch})
+
+(defn build-lifecycles []
+  [{:lifecycle/task :identity
+    :lifecycle/calls :lambdajam.challenge-4-3/aggregate-lifecycle
+    :onyx/doc "Computes an aggregate over the event stream"}
+
+   {:lifecycle/task :read-segments
+    :lifecycle/calls :lambdajam.challenge-4-3/reader-lifecycle
+    :core.async/id (java.util.UUID/randomUUID)
+    :onyx/doc "Injects the core.async reader channel"}
+
+   {:lifecycle/task :read-segments
+    :lifecycle/calls :onyx.plugin.core-async/reader-calls
+    :onyx/doc "core.async plugin base lifecycle"}
+
+   {:lifecycle/task :write-segments
+    :lifecycle/calls :lambdajam.challenge-4-3/writer-lifecycle
+    :core.async/id (java.util.UUID/randomUUID)
+    :onyx/doc "Injects the core.async writer channel"}
+
+   {:lifecycle/task :write-segments
+    :lifecycle/calls :onyx.plugin.core-async/writer-calls
+    :onyx/doc "core.async plugin base lifecycle"}])
+
+;; <<< END FILL ME IN >>>
