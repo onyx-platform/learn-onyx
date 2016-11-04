af4443adef103647600841a589a69d27a8f1e485
Michael Drogalis <mjd3089@rit.edu>
2016-11-04 15:25:23 -0700

    Fixed doc links.

--- test/workshop/jobs/challenge_3_0_test.clj
+++ test/workshop/jobs/challenge_3_0_test.clj
@@ -12,7 +12,7 @@
 ;; we learned earlier.
 ;;
 ;; Onyx has a chapter in its user guide dedicated to functions:
-;; http://www.onyxplatform.org/docs/user-guide/latest/functions.html
+;; http://www.onyxplatform.org/docs/user-guide/latest/#functions
 ;;
 ;; This challenge is an already-working example. Explore the functions
 ;; and comments in the corresponding source file about the contracts
