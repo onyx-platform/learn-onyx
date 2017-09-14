3ec237529e795609b9df29488ca121c179171f6e
OnyxBot <michael.drogalis@onyxplatform.org>
2017-09-14 02:51:28 +0000

    Update dependency org.onyxplatform/onyx to version 0.11.0.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.11.0-20170913_225512-gdd6739a"]
+                 [org.onyxplatform/onyx "0.11.0"]
                  [org.slf4j/slf4j-api "1.7.12"]
                  [org.slf4j/slf4j-nop "1.7.12"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]
